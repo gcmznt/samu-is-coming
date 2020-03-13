2bfe7a572ec1585663c7c39788a4af8d668a899e
Giacomo Zinetti <giacomo.zinetti@giko.it>
2020-03-13 10:21:40 +0100

    catch share cancel

--- src/components/Share.js
+++ src/components/Share.js
@@ -2,12 +2,14 @@ import React from "react";
 
 function action(e) {
   e.preventDefault();
-  window.navigator.share({
-    title: "È nato Samuele?",
-    text:
-      "Ti interessa sapere quando nascerà Samuele? Tieniti informato con questo link! Le linee telefoniche dei genitori sono intasate.",
-    url: "https://samu.laricettadellafelicita.it"
-  });
+  window.navigator
+    .share({
+      title: "È nato Samuele?",
+      text:
+        "Ti interessa sapere quando nascerà Samuele? Tieniti informato con questo link! Le linee telefoniche dei genitori sono intasate.",
+      url: "https://samu.laricettadellafelicita.it"
+    })
+    .catch(() => {});
 }
 
 export default function Share() {
